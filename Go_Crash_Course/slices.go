373760609c2a4e2aff7b2b4c5f72a7b92754519b
sureshkumar-cse <sureshkumar.cse@yahoo.com>
2024-11-11 13:26:24 +0530

    update the slice

--- Go_Crash_Course/slices.go
+++ Go_Crash_Course/slices.go
@@ -1,16 +1,19 @@
+// An array has a fixed size. A slice, on the other hand, is a dynamically-sized, flexible view into the elements of an array. In practice, slices are much more common than arrays.
+// The type []T is a slice with elements of type T.
+
 package main
 
 import "fmt"
 
 func main() {
-	var shortGolang = "Watch Go crash course"
-	var fullGolang = "Watch Golang Full Course"
-	var rewardDessert = "Reward myself with a chocolate"
 
-	var taskItems = []string {shortGolang, fullGolang, rewardDessert}
+	var taskItems = []string {"Watch Go crash course", "Watch Golang Full Course", "Reward myself with a chocolate"}
 
 	fmt.Println("##### Welcome to our Todolist App! #####")
 
 	fmt.Println("List of my Todos")
 	fmt.Println("Tasks:", taskItems)
+
+	var s []string = taskItems[1:3]
+	fmt.Println(s)
 }
