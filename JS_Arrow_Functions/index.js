a4f48d679836b00d3f77e90666de30de01b5f5d9
sureshkumar-cse <sureshkumar.cse@yahoo.com>
2024-12-26 06:15:17 +0530

    How to convert a Regular Function to an Arrow Function

--- JS_Arrow_Functions/index.js
+++ JS_Arrow_Functions/index.js
@@ -11,3 +11,30 @@ const greetings = (name) => {
 };
 
 greetings("Madhura");
+
+// How to Convert a Regular Function to an Arrow Function Easily
+function greetings(name) {
+  return `Hello, ${name}!`;
+}
+
+// Step 1: replace function with const
+const greetings(name) {
+  return `Hello, ${name}!`;
+}
+
+// Step 2: add = after the function name
+const greetings = (name) {
+  return `Hello, ${name}!`;
+}
+
+// Step 3: add => after the parentheses
+const greetings = (name) => {
+  return `Hello, ${name}!`;
+}
+
+
+// With a single line function, we can remove the curly brackets and the return keyword as follows:
+const greetings = (name) => `Hello, ${name}!`;
+
+// With exactly one parameter, we can also remove the parentheses:
+const greetings = name => `Hello, ${name}!`;
